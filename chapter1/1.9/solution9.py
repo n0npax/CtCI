51dd9011a9634d150ce4444ff78d77e1645819b8
Marcin Niemira <marcin.niemira@gmail.com>
2019-11-21 20:19:12 +1100

    lintfix

--- chapter1/1.9/solution9.py
+++ chapter1/1.9/solution9.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 def is_rotation(s1: str, s2: str) -> bool:
-    return is_substr(s1,s2+s2)
+    return is_substr(s1, s2 + s2)
+
 
 def is_substr(s1: str, s2: str):
     return s1 in s2
--- chapter1/1.9/test_solution9.py
+++ chapter1/1.9/test_solution9.py
@@ -5,11 +5,7 @@ from solution9 import is_rotation
 
 @pytest.mark.parametrize(
     "input_str1,input_str2,expected",
-    [
-        ('llama','lamal',True),
-        ('abcdef','cdefab',True),
-        ('bar','foo',False),
-    ],
+    [("llama", "lamal", True), ("abcdef", "cdefab", True), ("bar", "foo", False)],
 )
 def test_is_rotation(input_str1: str, input_str2: str, expected: str):
     assert is_rotation(input_str1, input_str2) == expected
